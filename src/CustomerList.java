b13f3b09c3b0749ab0d3e66077ff0aa18f4b0b6c
timmyha619 <71495186+timmyha619@users.noreply.github.com>
2021-09-01 19:15:16 +0700

    Add files via upload

---
+++ src/CustomerList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+
+public class CustomerList {
+	private ArrayList<Customer> theList;
+	
+	public CustomerList() {
+		this.theList = new ArrayList<Customer>();
+	}
+	
+	public ArrayList<Customer> getList() {
+		return this.theList;
+	}
+	
+	public Customer getCustomer(String id) {
+		for (int i = 0; i < this.theList.size(); i++) {
+			Customer temp = this.theList.get(i);
+			if (temp.getID().equals(id))
+				return temp;
+		}
+		System.out.println("Customer " + id + " not found");
+		return null;
+	}
+	
+	public boolean addCustomer(Customer cust) {
+		for (int i = 0; i < this.theList.size(); i++) {
+			Customer temp = this.theList.get(i);
+			if (temp.getID().equals(cust.getID()))
+				return false;
+		}
+		this.theList.add(cust);
+		return true;
+	}
+
+    public <SuperVIP> SuperVIP getSuperVIP(String ID) {
+    }
+}
---
+++ src/VideoStore.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+
+public class VideoStore<SuperVIP> {
+	private CustomerList customerList;
+	private VideoList videoList;
+	private SuperVIP superVIP;
+
+	public VideoStore() {
+		this.videoList = new VideoList();
+		this.customerList = new CustomerList();
+		this.superVIP = new SuperVIP();
+	}
+
+	public Video getVideo(String ID) {
+		return videoList.getVideo(ID);
+	}
+	
+	public boolean addVideo(Video vid) {
+		return videoList.addVideo(vid);
+	}
+	
+	public Customer getCustomer(String ID) {
+		return customerList.getCustomer(ID);
+	}
+
+	public SuperVIP getSuperVIP(String ID) {
+		return customerList.getSuperVIP(ID);
+	}
+
+
+
+	public boolean addCustomer(Customer cust) {
+		return customerList.addCustomer(cust);
+	}
+	
+	public static void main(String[] args) {
+		VideoStore myStore = new VideoStore();
+		//create 3 video items
+		Video vid = new Video("VD001", "Divergent", "Action", 1, false);
+		myStore.addVideo(vid);
+		vid = new Video("VD002", "Green Eggs and Ham", "Comedy", 1, false);
+		myStore.addVideo(vid);
+		vid = new Video("VD003", "Gone with the wind", "Drama", 2, false);
+		myStore.addVideo(vid);
+		
+		//create 3 customers
+		Customer cust = new SuperVIP("Ngo Bao Chau", "12 Math Avenue", "VIP001", "0203050813");
+		myStore.addCustomer(cust);
+		cust.borrowVid(myStore.getVideo("VD001"));
+		cust = new GuestCustomer("Pham Nhat Vuong", "12 Money Road", "G002", "0399999999");
+		myStore.addCustomer(cust);
+		cust.borrowVid(myStore.getVideo("VD002"));
+		cust = new GuestCustomer("Nguyen Xuan Phuc", "12 Politics Street", "G003", "0311112222");
+		myStore.addCustomer(cust);
+		cust.borrowVid(myStore.getVideo("VD003"));
+	}
+
+}
